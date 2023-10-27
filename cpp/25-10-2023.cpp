77bcf69ae8d3fddf9240e76aa6fc7ec5e6e90a1f
Leonardo Queiros <leonardoq.leao.atma@gmail.com>
2023-10-27 09:29:37 -0300

    chore: update

--- cpp/25-10-2023.cpp
+++ cpp/25-10-2023.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int kthGrammarWithPrint(int n, int k)
@@ -86,4 +87,29 @@ int kthGrammar(int n, int k)
     }
   }
   return isV1 ? v2[k - 1] : v[k - 1];
+}
+int kthGrammar2(int n, int k)
+{
+  vector<bool> v;
+  u_int cv = 0;
+  for (u_int i = 0; i < k; i++)
+  {
+    if (i == 0 || v[i] == 0)
+    {
+      v.push_back(0);
+      v.push_back(1);
+    }
+    else
+    {
+      v.push_back(1);
+      v.push_back(0);
+    }
+    cv++;
+    cv++;
+    if (cv > k)
+    {
+      return v[k - 1];
+    }
+  }
+  return 0;
 }
